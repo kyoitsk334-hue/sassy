3b19039e4aeb81470b3da9f348c44cf6c751992d
kyoitsk334-hue <kyo.itsk334@gmail.com>
2026-07-11 12:13:57 +0900

    Refactor file input handling to support multiple files

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ const fileList = document.getElementById("fileList");
 
 fileInput.addEventListener("change", function () {
 
-  const file = this.files[0];
+    for (const file of this.files) {
 
-  if (!file) return;
+        const url = URL.createObjectURL(file);
 
-  const card = document.createElement("div");
-  card.className = "card";
+        const card = document.createElement("div");
+        card.className = "card";
 
-  card.innerHTML = `
-    <div class="name">${file.name}</div>
-    <div class="type">${file.type || "ファイル"}</div>
-  `;
+        card.innerHTML = `
+            <div class="name">${file.name}</div>
+            <div class="type">${file.type}</div>
+        `;
 
-  fileList.prepend(card);
+        card.onclick = () => {
+            window.open(url, "_blank");
+        };
+
+        fileList.prepend(card);
+    }
+
+    this.value = "";
 
 });
